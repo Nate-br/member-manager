6e823898a2673443ac79ebd4e18747fc21cde053
Nate-br <snati6407@gmail.com>
2025-10-07 20:03:20 +0300

    Delete telegram-bot.js

--- telegram-bot.js
+++
@@ -1,9 +0,0 @@
-// telegram-bot.js
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot('YOUR_TELEGRAM_BOT_TOKEN', { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  console.log("New user chat ID:", chatId);
-  bot.sendMessage(chatId, "Welcome! Please enter your phone number to log in.");
-});
